a0abc6d2d194e6a8e04b1694924c472e49c59e4e
ThinhNgVhust <45223680+ThinhNgVhust@users.noreply.github.com>
2021-12-15 10:07:22 +0900

    Update 6_Power of Two.py

--- Orange/W2_BitManipulation/6_Power of Two.py
+++ Orange/W2_BitManipulation/6_Power of Two.py
@@ -1,15 +1,25 @@
-def check_p_o_t(num):
-    return num & (num - 1) == 0
+def check(num):
+    return num > 0 and (num & (num - 1) == 0)
+
+
+def solve_one():
+    input()
+    arr = list(map(int, input().split()))
+    for i in range(32):
+        ans = 0xffffffff
+        for e in arr:
+            if e & (1 << i) != 0:
+                ans = ans & e
+                if check(ans) is True:
+                    print("YES")
+                    return
+    print("NO")
 
 
 def solver():
     T = int(input())
     for _ in range(T):
-        n = input()
-        arr = list(map(int, input().split()))
-        for i in range(64):
-            ans = 0xffffffff
-            print(ans)
+        solve_one()
 
 
 solver()
